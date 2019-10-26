e45b5a96c4923583907cb38b1927c0a5a1d78a43
Miles Bardon <miles.bardon@tesco.com>
2019-10-26 19:05:10 +0100

    Added authorised request to the card-generator

--- yugiohbot/main.py
+++ yugiohbot/main.py
@@ -1,5 +1,7 @@
 from text import *
 from title import *
+import google.auth
+from google.auth.transport.requests import AuthorizedSession
 
 
 def function(event, context):
@@ -14,7 +16,12 @@ def function(event, context):
     result = {'title': card_title, 'text': card_text}
     print(result)
 
-    return result
+    credentials, project = google.auth.default()
+    authed_session = AuthorizedSession(credentials)
+
+    response = authed_session.get("https://yugiohbot-card-generator-t4loex5l4q-ue.a.run.app", params=result)
+
+    return response
 
 
 if __name__ == '__main__':

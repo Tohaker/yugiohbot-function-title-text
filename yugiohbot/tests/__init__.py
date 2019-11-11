1308aac88a8e81c92f5359abcb4293a3868a202d
milesbardon <milesbardon@gmail.com>
2019-11-11 22:01:29 +0000

    Changing to use env variables for testing

--- yugiohbot/tests/__init__.py
+++ yugiohbot/tests/__init__.py
@@ -1,4 +1,4 @@
 import os
 
-os.environ['TEST_FLAVOUR'] = '../resources/flavour_list.csv'
+os.environ['TEST_FLAVOUR'] = 'yugiohbot/resources/flavour_list.csv'
 os.environ['TEST_ML'] = 'yugiohbot/resources/model/outputs/{}'
